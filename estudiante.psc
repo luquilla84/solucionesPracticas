58698af273b2c148db75b02c7cde031dc8c06630
lucas <lucassebastianbrocanelli@gmail.com>
2024-05-23 22:13:59 -0300

    Primer commit

---
+++ estudiante.psc
@@ -0,0 +1,28 @@
+Algoritmo estudiante // ingreso e datos
+	Escribir "la nota de la materia 1 es: "
+	Leer nota1
+	Escribir  "la nota de la materia 2 es: "
+	Leer nota2
+	Escribir "la nota de la materia 3 es: "
+	Leer nota3
+	Escribir "la nota de la materia 4 es: "
+	Leer nota4
+	Escribir  "la nota de la materia 5 es: "
+	Leer nota5
+	
+	// calculos para llegar al resultado
+	sumaTotalNotas = nota1 + nota2 + nota3 + nota4 + nota5
+	Escribir  "la suma total de las notas es : ", sumaTotalNotas
+	
+	cantidadNotas = 5
+	Escribir "La cantidad de nota es: ",cantidadNotas
+	
+	promedio = sumaTotalNotas / cantidadNotas
+	
+	//resultado
+	
+	Escribir "El promedio de las notas es: ", promedio
+	
+	
+	
+FinAlgoritmo
---
+++ estudiante.py
@@ -0,0 +1,35 @@
+nota1 = float(input("introduce la nota de la materia 1: "))
+nota2 = float(input("introduce la nota de la materia 2:  "))
+nota3 = float(input("introduce la nota de la materia 3: "))
+nota4 = float(input("introduce la nota de la materia 4: "))
+nota5 = float(input("introduce la nota de la materia 5: "))
+
+sumaTotal = 0
+contador = 0
+
+if nota1 >= 0:
+    sumaTotal += nota1
+    contador += 1
+
+if nota2 >= 0:
+    sumaTotal += nota2
+    contador += 1
+
+if nota3 >= 0:
+    sumaTotal += nota3
+    contador += 1
+
+if nota4 >= 0:
+    sumaTotal += nota4
+    contador += 1   
+
+if nota5 >= 0:
+    sumaTotal += nota5
+    contador += 1
+
+if contador > 0:
+    promedio = sumaTotal / contador
+    print(f"la suma de las notas es: {sumaTotal}, y el promedio de las notas es: {promedio} ")
+
+
+
